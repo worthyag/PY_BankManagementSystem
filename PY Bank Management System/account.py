b0bdc0dfd8ae1f43fde183508d72a4e6470f89ef
Worthy <worthyintech@gmail.com>
2023-12-08 14:10:31 +0000

    Updating the account class.

---
+++ PY Bank Management System/account.py
@@ -0,0 +1,23 @@
+class Account:
+    def __init__(self, id):
+        self.user_id = id
+        self.account_number = None
+        self.balance = 0
+
+    def viewBalance(self):
+        pass
+
+    def depositMoney(self):
+        pass
+
+    def withdrawMoney(self):
+        pass
+
+    def transferMoney(self):
+        pass
+
+    def viewAccountInfo(self):
+        pass
+
+    def updateAccountInfo(self):
+        pass
