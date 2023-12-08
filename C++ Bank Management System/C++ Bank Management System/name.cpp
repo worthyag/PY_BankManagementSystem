13146cf8a117bd0a5944f87a7ddbc482dc1a13fb
Worthy <worthyintech@gmail.com>
2023-12-08 12:55:57 +0000

    Adding the name file.

---
+++ C++ Bank Management System/C++ Bank Management System/name.cpp
@@ -0,0 +1,8 @@
+//
+//  name.cpp
+//  C++ Bank Management System
+//
+//  Created by Worthy on 08/12/2023.
+//
+
+#include "name.hpp"
---
+++ C++ Bank Management System/C++ Bank Management System/name.hpp
@@ -0,0 +1,13 @@
+//
+//  name.hpp
+//  C++ Bank Management System
+//
+//  Created by Worthy on 08/12/2023.
+//
+
+#ifndef name_hpp
+#define name_hpp
+
+#include <stdio.h>
+
+#endif /* name_hpp */
