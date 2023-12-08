b1b786d6be7f6f7f7a4cee5e3e288a2066b3539f
Worthy <worthyintech@gmail.com>
2023-12-08 14:37:33 +0000

    Updating progress.

--- PY Bank Management System/account.py
+++ PY Bank Management System/account.py
@@ -2,7 +2,7 @@ class Account:
     def __init__(self, id: str) -> None:
         self.user_id: str = id
         self.account_number: str = self.generate_account_number()
-        self.balance: float = 0
+        self.balance: float = 0.0
 
     def generate_account_number(self) -> str:
         pass
