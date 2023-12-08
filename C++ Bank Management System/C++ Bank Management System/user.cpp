b53906371e512df4afd8fc672420c96e264dbc1a
Worthy <worthyintech@gmail.com>
2023-12-08 13:15:25 +0000

    Adding the user file.

---
+++ C++ Bank Management System/C++ Bank Management System/user.cpp
@@ -0,0 +1,8 @@
+//
+//  user.cpp
+//  C++ Bank Management System
+//
+//  Created by Worthy on 08/12/2023.
+//
+
+#include "user.hpp"
---
+++ C++ Bank Management System/C++ Bank Management System/user.hpp
@@ -0,0 +1,13 @@
+//
+//  user.hpp
+//  C++ Bank Management System
+//
+//  Created by Worthy on 08/12/2023.
+//
+
+#ifndef user_hpp
+#define user_hpp
+
+#include <stdio.h>
+
+#endif /* user_hpp */
