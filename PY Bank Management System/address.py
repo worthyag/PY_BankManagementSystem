2df25cc53a53a10fa6904b44a4d2dd4d1727c39c
Worthy <worthyintech@gmail.com>
2023-12-08 14:30:30 +0000

    Working on the address class.

---
+++ PY Bank Management System/address.py
@@ -0,0 +1,9 @@
+class Address:
+    def __init__(self, country: str, state: str, city: str,
+                 street_address: str, post_code: str, user_id: str) -> None:
+        self.country: str = country
+        self.state: str = state
+        self.city: str = city
+        self.street_address: str = street_address
+        self.post_code: str = post_code
+        self.user_id: str = user_id
